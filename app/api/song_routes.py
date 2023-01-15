55b9bc58a97999f0e0454a8dd182e1b7988e9f5a
Anthony <avithayathil23@gmail.com>
2023-01-14 19:28:26 -0500

    get all songs route done

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -4,9 +4,9 @@ from app.models import Song
 
 song_routes = Blueprint('songs', __name__)
 
-song_routes.route('/')
+@song_routes.route('/')
 def all_songs():
     songs = Song.query.all()
-    print(songs)
+    #print(list(songs))
 
-    return {"songs": [song.to_dict()] for song in songs}
+    return {'songs' :[song.to_dict() for song in songs]}
