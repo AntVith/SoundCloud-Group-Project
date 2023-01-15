0e41caef1fa3201999ffc53f4b28b4f7fbdac292
Anthony <avithayathil23@gmail.com>
2023-01-14 19:43:14 -0500

    song store file created

---
+++ react-app/src/store/songs.js
@@ -0,0 +1,14 @@
+// import {csrFetch} from './'
+
+const GET_ALL_SONGS = 'song/GET_ALL_SONGS'
+
+// const getAllSongs = ()
+
+initialState = {
+
+}
+
+const songsReducer = {}
+
+
+export default songsReducer
