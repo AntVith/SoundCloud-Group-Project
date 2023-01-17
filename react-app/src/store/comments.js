a5ffac71a4a4d3463c14e2d270a0446df9aef4bf
Anthony <avithayathil23@gmail.com>
2023-01-16 23:26:40 -0500

    finsihed post a comment

--- react-app/src/store/comments.js
+++ react-app/src/store/comments.js
@@ -1,9 +1,32 @@
 const GET_ALL_COMMENTS = 'comment/GET_ALL_COMMENTS'
+const POST_COMMENT = 'comment/POST_COMMENT'
 
 const getAll = (comments) => ({
     type: GET_ALL_COMMENTS,
     comments
 })
+
+const postComment = (comment) => ({
+  type: POST_COMMENT,
+  comment
+})
+
+export const postAComment = (id, payload) => async(dispatch) => {
+  const response = await fetch(`/api/songs/${id}/comments/new`, {
+
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(payload)
+  })
+
+  if (response.ok){
+    const newComment = await response.json()
+
+    dispatch(postComment(newComment))
+    return newComment
+  }
+
+}
 
 
 export const getAllComments = (id) => async (dispatch) => {
@@ -32,6 +55,13 @@ export const getAllComments = (id) => async (dispatch) => {
          newState.comments = newObject
          return newState
 
+        }
+        case POST_COMMENT: {
+          const newState = {...state}
+          const newObject = {...state.comments}
+          newObject[action.comment.id] = action.comment
+          newState.comments = newObject
+          return newState
         }
 
 
