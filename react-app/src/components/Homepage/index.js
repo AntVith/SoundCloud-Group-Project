d3e904c3fad1b8f704886e1f9b673955ab60e01f
kalebrobi <kalebrobi10@gmail.com>
2023-01-19 11:51:24 -0700

    divs for homepage

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -21,20 +21,71 @@ const HomePage = () => {
   return (
     <section className='hompage-container'>
       <div className='main-container'>
+        <div className='genre-container'>Soul
+
+        </div>
+
+        <div className='genre-container'>Rock
+
+        </div>
+
+        <div className='genre-container'>R&B
+
+        </div>
+
+        <div className='genre-container'>Rap
+
+        </div>
+
+        <div className='genre-container'>Pop
+
+        </div>
+
+        <div className='genre-container'>Jazz
+
+        </div>
+
+        <div className='genre-container'>Instrumental
+
+        </div>
+
+        <div className='genre-container'>Hip-Hop
+
+        </div>
+
+        <div className='genre-container'>Gospel
+
+        </div>
+
+        <div className='genre-container'>EDM
+
+        </div>
+
+        <div className='genre-container'>Drill
+
+        </div>
+
+        <div className='genre-container'>Disco
+
+        </div>
+
+        <div className='genre-container'>Country
+
+        </div>
+
+        <div className='genre-container'>Classical
+
         {
           songs.map(song => (
-
               <NavLink
                 to={`/songs/${song.id}`}
                 key={song.id}
                 style={{textDecoration: 'none'}}>
                <div>{song.song_title}</div>
-
               </NavLink>
-
-
           ))
         }
+        </div>
       </div>
 
     </section>
