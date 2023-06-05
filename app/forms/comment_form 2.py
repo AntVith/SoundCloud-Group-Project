820f18b2d2eae6a6753ac0097778d18d912775cb
Tyler Malinoski <tmalinoski1@gmail.com>
2023-06-04 23:06:35 -0700

    deleted duplicate files

--- app/forms/comment_form 2.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, BooleanField
-from wtforms.validators import DataRequired
-
-class CommentForm(FlaskForm):
-    song_id = IntegerField('Song Id', validators=[DataRequired()])
-    user_id = StringField('User Id', validators=[DataRequired()])
-    comment = StringField('Comment', validators=[DataRequired()])
